2072e989368287f53603954b893c5c86ce2909cd
Omgkiller <39164044+Omgkiller@users.noreply.github.com>
2026-08-20 14:03:53 +0800

    Add: longest-repeating-substring-with-replacement - submission-2

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-2.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int[] unique = new int[26];
+        int i = 0;
+        int j = 0;
+        int longest = 0;
+        int maxFreq = 0;
+        while (j < s.length()){
+            int r = s.charAt(j) - 'A';
+            maxFreq = Math.max(maxFreq, ++unique[r]);
+            j++;
+            while (j-i -maxFreq > k){
+                unique[s.charAt(i)-'A']--;
+                i++;
+            }
+            longest = Math.max(longest, j-i);
+        }
+        return longest;
+    }
+
+    private int getMostFrequent(int[] unique){
+        int result = 0;
+        for (int i = 0; i<26; i++){
+            result = Math.max(result, unique[i]);
+        }
+        return result;
+    }
+}
